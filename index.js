94d348d801311eea9f29b056a9047da2f58f947b
Yildiz Zor Unlu <yildizzor@gmail.com>
2023-10-14 01:58:54 +0200

    Completed

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
+mongoose.connect(MONGODB_URI).then((x) => {
+  console.log(`Connected to the database: "${x.connection.name}"`);
+  // Before adding any recipes to the database, let's remove all existing ones
+  return Recipe.deleteMany();
+});
+
+const myRecipe = {
+  title: "Apple Tart",
+  level: "Amateur Chef",
+  ingredients: [
+    "flour, for dusting",
+    "375g/13oz block dessert pastry",
+    "2 tbsp ground almonds",
+    "3 tbsp golden caster sugar",
+    "3 eating apples, peeled, cored and halved, then each half thinly sliced",
+    "50g butter",
+    "icing sugar, to serve (optional)",
+    "vanilla ice cream, to serve",
+  ],
+  cuisine: "Turkish",
+  dishType: "dessert",
+  image:
+    "https://rutgerbakt.nl/wp-content/uploads/2018/10/appelkruimeltaart-e1495794621103_600x900_acf_cropped-1024x0-c-default.jpg",
+  duration: 90,
+  creator: "Chef Yildiz",
+};
+
+Recipe.create(myRecipe)
+  .then((recipe) => {
     // Run your code here, after you have insured that the connection was made
+
+    console.log(`${recipe.title} is title of the recipe`);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error during recipe creation", error);
   });
+
+Recipe.insertMany(data)
+  .then((recipes) => {
+    recipes.forEach((recipe) => {
+      console.log(`${recipe.title} is inserted into database.`);
+    });
+  })
+
+  .catch((error) => {
+    console.error("Recipe is not insert the database: ", error);
+  });
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+
+  .then(() => console.log("Rigatoni alla Genovese recipe duration is updated."))
+  .catch((error) => console.error(`Recipe can't be updated:`, error));
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+
+  .then(() => console.log("Carrot Cake is deleted."))
+  .catch((error) => console.error(`Recipe can't be deleted:`, error));
+
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  });
+});
