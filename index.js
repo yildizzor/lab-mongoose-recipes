cdc859d522629dde6551e46f22c1e2b480851eaf
Yildiz Zor Unlu <yildizzor@gmail.com>
2023-10-14 02:15:53 +0200

    Updated

--- index.js
+++ index.js
@@ -8,69 +8,68 @@ const data = require("./data");
 const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose.connect(MONGODB_URI).then((x) => {
-  console.log(`Connected to the database: "${x.connection.name}"`);
-  // Before adding any recipes to the database, let's remove all existing ones
-  return Recipe.deleteMany();
-});
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing ones
+    return Recipe.deleteMany();
+  })
+  .then(() => {
+    const myRecipe = {
+      title: "Apple Tart",
+      level: "Amateur Chef",
+      ingredients: [
+        "flour, for dusting",
+        "375g/13oz block dessert pastry",
+        "2 tbsp ground almonds",
+        "3 tbsp golden caster sugar",
+        "3 eating apples, peeled, cored and halved, then each half thinly sliced",
+        "50g butter",
+        "icing sugar, to serve (optional)",
+        "vanilla ice cream, to serve",
+      ],
+      cuisine: "Turkish",
+      dishType: "dessert",
+      image:
+        "https://rutgerbakt.nl/wp-content/uploads/2018/10/appelkruimeltaart-e1495794621103_600x900_acf_cropped-1024x0-c-default.jpg",
+      duration: 90,
+      creator: "Chef Yildiz",
+    };
 
-const myRecipe = {
-  title: "Apple Tart",
-  level: "Amateur Chef",
-  ingredients: [
-    "flour, for dusting",
-    "375g/13oz block dessert pastry",
-    "2 tbsp ground almonds",
-    "3 tbsp golden caster sugar",
-    "3 eating apples, peeled, cored and halved, then each half thinly sliced",
-    "50g butter",
-    "icing sugar, to serve (optional)",
-    "vanilla ice cream, to serve",
-  ],
-  cuisine: "Turkish",
-  dishType: "dessert",
-  image:
-    "https://rutgerbakt.nl/wp-content/uploads/2018/10/appelkruimeltaart-e1495794621103_600x900_acf_cropped-1024x0-c-default.jpg",
-  duration: 90,
-  creator: "Chef Yildiz",
-};
-
-Recipe.create(myRecipe)
+    return Recipe.create(myRecipe);
+  })
   .then((recipe) => {
     // Run your code here, after you have insured that the connection was made
 
     console.log(`${recipe.title} is title of the recipe`);
-  })
-  .catch((error) => {
-    console.error("Error during recipe creation", error);
-  });
 
-Recipe.insertMany(data)
+    return Recipe.insertMany(data);
+  })
+
   .then((recipes) => {
     recipes.forEach((recipe) => {
       console.log(`${recipe.title} is inserted into database.`);
     });
+
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
   })
 
-  .catch((error) => {
-    console.error("Recipe is not insert the database: ", error);
-  });
+  .then(() => {
+    console.log("Rigatoni alla Genovese recipe duration is updated.");
 
-Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
-
-  .then(() => console.log("Rigatoni alla Genovese recipe duration is updated."))
-  .catch((error) => console.error(`Recipe can't be updated:`, error));
-
-Recipe.deleteOne({ title: "Carrot Cake" })
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
 
   .then(() => console.log("Carrot Cake is deleted."))
-  .catch((error) => console.error(`Recipe can't be deleted:`, error));
-
-process.on("SIGINT", () => {
-  mongoose.connection.close(() => {
-    console.log(
-      "Mongoose default connection disconnected through app termination"
-    );
-    process.exit(0);
-  });
-});
+  .catch((error) => console.error(`Recipe can't be deleted:`, error))
+  .finally(
+    mongoose.connection.close(() => {
+      console.log(
+        "Mongoose default connection disconnected through app termination"
+      );
+    })
+  );
